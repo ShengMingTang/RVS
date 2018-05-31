fca46cff219c868f4d050e32f94fffab0b463b66
Kroon, Bart <bart.kroon@philips.com>
2018-05-31 09:23:27 +0200

    Working on CTC configuration files

--- extra/JSON_to_SVS_and_VSRS.m
+++ extra/JSON_to_SVS_and_VSRS.m
@@ -1,6 +1,6 @@
 % ------------------------------------------------------------------------------ -
 % 
-% Copyright © 2018 Koninklijke Philips N.V.
+% Copyright ? 2018 Koninklijke Philips N.V.
 % 
 % Authors : Bart Kroon, Bart Sonneveldt
 % Contact : bart.kroon@philips.com
@@ -239,7 +239,7 @@ fprintf(svs_file, 'Precision 2.0\n\n');
 fprintf(svs_file, 'ColorSpace YUV\n\n');
 fprintf(svs_file, 'ViewSynthesisMethod Triangles\n\n');
 fprintf(svs_file, 'BlendingMethod Simple\n\n');
-fprintf(svs_file, 'BlendingFactor 1.0\n\n');
+fprintf(svs_file, 'BlendingFactor 5.0\n\n'); % suggested by Sarah
 fprintf(svs_file, 'SensorSize 1.0\n\n');
 fclose(svs_file);
 
