39cb49eb18c512372c07e3b33eb0d63f3999cf8a
Bart Kroon <bart.kroon@philips.com>
2018-06-26 15:55:27 +0200

    Refactored transform.cp

--- src/transform.cpp
+++ src/transform.cpp
@@ -33,6 +33,7 @@ of the 3DoF+ Investigation.
 Modifications copyright © 2018 Koninklijke Philips N.V.
 
 Extraction of a generalized unproject -> translate/rotate -> project flow
+Fixes and refactoring
 
 Author  : Bart Kroon, Bart Sonneveldt
 Contact : bart.kroon@philips.com
@@ -50,11 +51,7 @@ extern float rescale; // Config.hpp
 
 namespace
 {
-	float valid_tri(const cv::Mat & new_pos, cv::Vec2f a, cv::Vec2f b, cv::Vec2f c, float /*den*/) {
-		cv::Vec2f A = new_pos.at<cv::Vec2f>((int)a[1], (int)a[0]);
-		cv::Vec2f B = new_pos.at<cv::Vec2f>((int)b[1], (int)b[0]);
-		cv::Vec2f C = new_pos.at<cv::Vec2f>((int)c[1], (int)c[0]);
-
+	float valid_tri(cv::Vec2f A, cv::Vec2f B, cv::Vec2f C) {
 		double dab = cv::norm(A, B);
 		double dac = cv::norm(A, C);
 		double dbc = cv::norm(B, C);
@@ -69,66 +66,74 @@ namespace
 		return quality;
 	}
 
-	void colorize_triangle(const cv::Mat & img, const cv::Mat & depth, const cv::Mat& depth_prologation_mask, const cv::Mat & new_pos, cv::Mat& res, cv::Mat& depth_inv, cv::Mat& new_depth_prologation_mask, cv::Mat& triangle_shape, cv::Vec2i a, cv::Vec2i b, cv::Vec2i c) {
-		//	float w = (float)img.cols; -- unused variable
-		//	float h = (float)img.rows; -- unused variable
-		cv::Vec2f A = new_pos.at<cv::Vec2f>(a[1], a[0]);
-		cv::Vec2f B = new_pos.at<cv::Vec2f>(b[1], b[0]);
-		cv::Vec2f C = new_pos.at<cv::Vec2f>(c[1], c[0]);
-		cv::Vec3f colA = img.at<cv::Vec3f>(a[1], a[0]);
-		cv::Vec3f colB = img.at<cv::Vec3f>(b[1], b[0]);
-		cv::Vec3f colC = img.at<cv::Vec3f>(c[1], c[0]);
-		float dA = (depth.at<float>(a[1], a[0]));
-		float dB = (depth.at<float>(b[1], b[0]));
-		float dC = (depth.at<float>(c[1], c[0]));
+	void colorize_triangle(const cv::Mat & img, const cv::Mat & depth, const cv::Mat& depth_prologation_mask, const cv::Mat & new_pos, cv::Mat& res, cv::Mat& new_depth, cv::Mat& new_depth_prologation_mask, cv::Mat& triangle_shape, cv::Point a, cv::Point b, cv::Point c) {
+		cv::Vec2f A = new_pos.at<cv::Vec2f>(a);
+		cv::Vec2f B = new_pos.at<cv::Vec2f>(b);
+		cv::Vec2f C = new_pos.at<cv::Vec2f>(c);
+		
 		float den = (B[1] - C[1]) * (A[0] - C[0]) + (C[0] - B[0]) * (A[1] - C[1]);
-		if (den <= 0.0f)
+		if (den <= 0.f)
 			return;
-		float triangle_validity = valid_tri(new_pos, a, b, c, den)/* std::powf(MAX(MAX(abs(dA-dB),abs(dB-dC)),abs(dC-dA)), 1.0 / 8.0)*/;
-		if (triangle_validity == 0.0)
+		
+		float triangle_validity = valid_tri(A, B, C);
+		if (triangle_validity == 0.f)
 			return;
-		//triangle_validity /= std::powf(MAX(MAX(dA, dB), dC) - MIN(MIN(dA, dB), dC), 1.0 / 8.0) / 1000.0;
-		float offset = 0.5;
+		
 		auto Xmin = std::max(0, static_cast<int>(std::floor(std::min(std::min(A[0], B[0]), C[0]))));
 		auto Ymin = std::max(0, static_cast<int>(std::floor(std::min(std::min(A[1], B[1]), C[1]))));
 		auto Xmax = std::min(res.cols - 1, static_cast<int>(std::ceil(std::max(std::max(A[0], B[0]), C[0]))));
 		auto Ymax = std::min(res.rows - 1, static_cast<int>(std::ceil(std::max(std::max(A[1], B[1]), C[1]))));
+
+		if (Ymin > Ymax || Xmin > Xmax)
+			return;
+
+		cv::Vec3f colA = img.at<cv::Vec3f>(a);
+		cv::Vec3f colB = img.at<cv::Vec3f>(b);
+		cv::Vec3f colC = img.at<cv::Vec3f>(c);
+		float dA = depth.at<float>(a);
+		float dB = depth.at<float>(b);
+		float dC = depth.at<float>(c);
+
 		for (int y = Ymin; y <= Ymax; ++y)
 			for (int x = Xmin; x <= Xmax; ++x)
-				{
-					float lambda_1 = ((B[1] - C[1]) * ((float)x + offset - C[0]) + (C[0] - B[0]) * ((float)y + offset - C[1])) / den;
-					float lambda_2 = ((C[1] - A[1]) * ((float)x + offset - C[0]) + (A[0] - C[0]) * ((float)y + offset - C[1])) / den;
-					float lambda_3 = 1.0f - lambda_1 - lambda_2;
-					float const eps = 1e-6f;
-					if (lambda_1 >= -eps && lambda_2 >= -eps && lambda_3 >= -eps)
-					{
-						cv::Vec3f col = colA*lambda_1 + colB*lambda_2 + colC*lambda_3;
-						float d = dA*lambda_1 + dB*lambda_2 + dC*lambda_3;
+			{
+				float const offset = 0.5f;
+				float lambda_1 = ((B[1] - C[1]) * ((float)x + offset - C[0]) + (C[0] - B[0]) * ((float)y + offset - C[1])) / den;
+				float lambda_2 = ((C[1] - A[1]) * ((float)x + offset - C[0]) + (A[0] - C[0]) * ((float)y + offset - C[1])) / den;
+				float lambda_3 = 1.f - lambda_1 - lambda_2;
 
-						//if the pixel comes from original depth map and is in foreground
-						if ((depth_inv.at<float>(y, x)*depth_inv.at<float>(y, x)*depth_inv.at<float>(y, x)* triangle_shape.at<float>(y, x) < 1.0 / d / d / d * triangle_validity
-							|| new_depth_prologation_mask.at<bool>(y, x)) && !depth_prologation_mask.at<bool>((int)a[1], (int)a[0])
-							&& !depth_prologation_mask.at<bool>((int)b[1], (int)b[0])
-							&& !depth_prologation_mask.at<bool>((int)c[1], (int)c[0]))
-						{
-							depth_inv.at<float>(y, x) = 1.0f / d;
-							new_depth_prologation_mask.at<bool>(y, x) = false;
-							triangle_shape.at<float>(y, x) = triangle_validity;
-							res.at<cv::Vec3f>(y, x) = col;
-						}
-						//if the pixel comes from inpainted depth map and is in foreground and there is no pixel from the original depth map
-						else if (depth_inv.at<float>(y, x)*depth_inv.at<float>(y, x)*depth_inv.at<float>(y, x)* triangle_shape.at<float>(y, x) < 1.0 / d / d / d * triangle_validity
-							&& new_depth_prologation_mask.at<bool>(y, x)
-							&& (depth_prologation_mask.at<bool>((int)a[1], (int)a[0])
-								|| depth_prologation_mask.at<bool>((int)b[1], (int)b[0])
-								|| depth_prologation_mask.at<bool>((int)c[1], (int)c[0])))
-						{
-							depth_inv.at<float>(y, x) = 1.0f / d;
-							triangle_shape.at<float>(y, x) = triangle_validity;
-							res.at<cv::Vec3f>(y, x) = col;
-						}
+				float const eps = 1e-6f;
+				if (lambda_1 >= -eps && lambda_2 >= -eps && lambda_3 >= -eps)
+				{
+					cv::Vec3f col = colA*lambda_1 + colB*lambda_2 + colC*lambda_3;
+					float d = dA*lambda_1 + dB*lambda_2 + dC*lambda_3;
+
+					auto& new_d = new_depth.at<float>(y, x);
+					auto& shape = triangle_shape.at<float>(y, x);
+					auto ratio = d / new_d;
+					auto is_valid = ratio * ratio * ratio * shape < triangle_validity;
+					auto& new_prol = new_depth_prologation_mask.at<bool>(y, x);
+					auto prol = depth_prologation_mask.at<bool>(a) 
+						     || depth_prologation_mask.at<bool>(b) 
+						     || depth_prologation_mask.at<bool>(c);
+
+					//if the pixel comes from original depth map and is in foreground
+					if ((is_valid || new_prol) && !prol)
+					{
+						new_d = d;
+						new_prol = false;
+						shape = triangle_validity;
+						res.at<cv::Vec3f>(y, x) = col;
+					}
+					//if the pixel comes from inpainted depth map and is in foreground and there is no pixel from the original depth map
+					else if (is_valid && new_prol && prol)
+					{
+						new_d = d;
+						shape = triangle_validity;
+						res.at<cv::Vec3f>(y, x) = col;
 					}
 				}
+			}
 	}
 } // namespace
 
@@ -138,7 +143,7 @@ cv::Mat3f transform_trianglesMethod(cv::Mat3f input_color, cv::Mat1f input_depth
     int w = input_size.width;
 	cv::Mat1b input_depth_mask = input_depth > 0.f;
 	cv::Mat3f color = cv::Mat3f::zeros(output_size);
-	cv::Mat1f depth_inv = cv::Mat1f::zeros(output_size);
+	depth = cv::Mat1f(output_size, std::numeric_limits<float>::infinity());
 	triangle_shape = cv::Mat1f::zeros(output_size);
 	cv::Mat1b new_depth_prologation_mask = cv::Mat1b::ones(output_size);
 
@@ -147,28 +152,22 @@ cv::Mat3f transform_trianglesMethod(cv::Mat3f input_color, cv::Mat1f input_depth
 		for (int j = 0; j < input_size.width - 1; ++j) {
 			if (input_depth(i, j + 1) > 0.f && input_depth(i + 1, j) > 0.f && /*why?*/ input_positions(i, j + 1)[0] > 0.f && /*why?*/ input_positions(i + 1, j)[0] > 0.f) {
 				if (input_depth(i, j) > 0.f && /*why?*/ input_positions(i, j)[0] > 0.f)
-					colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth_inv, new_depth_prologation_mask, triangle_shape,
-						/*why float?*/ cv::Vec2i(j, i), cv::Vec2i(j + 1, i), cv::Vec2i(j, i + 1));
+					colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth, new_depth_prologation_mask, triangle_shape,
+						/*why float?*/ cv::Point(j, i), cv::Point(j + 1, i), cv::Point(j, i + 1));
 				if (input_depth(i + 1, j + 1) > 0.f && input_positions(i + 1, j + 1)[0] > 0.f)
-					colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth_inv, new_depth_prologation_mask, triangle_shape,
-						/*why float?*/ cv::Vec2i(j + 1, i + 1), cv::Vec2i(j, i + 1), cv::Vec2i(j + 1, i));
+					colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth, new_depth_prologation_mask, triangle_shape,
+						/*why float?*/ cv::Point(j + 1, i + 1), cv::Point(j, i + 1), cv::Point(j + 1, i));
 			
                 // stitch left and right borders with triangles (e.g. for equirectangular projection)
                 if( horizontalWrap && j == 0 ) { 
-                    colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth_inv, new_depth_prologation_mask, triangle_shape,
-                        cv::Vec2i(w-1, i), cv::Vec2i( 0, i), cv::Vec2i(w-1, i + 1));
-                    colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth_inv, new_depth_prologation_mask, triangle_shape, 
-                        cv::Vec2i(0, i + 1),   cv::Vec2i(w-1, i + 1), cv::Vec2i(0, i));
+                    colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth, new_depth_prologation_mask, triangle_shape,
+                        cv::Point(w-1, i), cv::Point( 0, i), cv::Point(w-1, i + 1));
+                    colorize_triangle(input_color, input_depth, input_depth_mask, input_positions, color, depth, new_depth_prologation_mask, triangle_shape, 
+                        cv::Point(0, i + 1),   cv::Point(w-1, i + 1), cv::Point(0, i));
                 }
             }
 		}
 	}
-
-	// Memory optimization: release memory prior to allocation
-	input_depth_mask.release();
-	new_depth_prologation_mask.release();
-
-	depth = 1.f / depth_inv;
 
 	return color;
 }
